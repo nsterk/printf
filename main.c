677a35ece1544825201c5d849778380036a20998
Naomi Sterk <nsterk@f0r4s6.codam.nl>
2020-11-30 21:12:13 +0100

    kut

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/28 20:20:06 by nsterk        #+#    #+#                 */
-/*   Updated: 2020/11/30 19:55:37 by nsterk        ########   odam.nl         */
+/*   Updated: 2020/11/30 19:59:02 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,13 @@ int		main(void)
 	// ret2 = printf("[%.*i]\n", -50, i);  
 	
 	i = 0;
-	// ret = ft_printf("mine:	[%010x] [%0*x]\n", i, -10, i);
 	ret = ft_printf("mine:	[%010x] [%0*x]\n", i, -10, i);
 	ret2 = printf("real:	[%010x] [%0*x]\n", i, -10, i);
 	if (ret != ret2)
 		printf("\e[0;31mreturn value incorrect\e[0m\nexpected: %i\nreceived: %i\n", ret2, ret);
 	
-	// ret = ft_printf("%.i\n", i);
-	// ret2 = printf("%.i\n", i);
+	// ret = ft_printf("%s\n", str);
+	// ret2 = printf("%s\n", str);
 	// if (ret != ret2)
 	// 	printf("\e[0;31mreturn value incorrect\e[0m\nexpected: %i\nreceived: %i\n", ret2, ret);
 	
