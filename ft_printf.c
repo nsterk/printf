456d962de387ef751d8487500a0e66273b637e82
Naomi <nsterk@student.codam.nl>
2021-01-12 19:50:20 +0100

    fix free tab if fail

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/09 20:32:00 by nsterk        #+#    #+#                 */
-/*   Updated: 2020/12/20 16:40:33 by nsterk        ########   odam.nl         */
+/*   Updated: 2021/01/12 19:49:44 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ int		ft_printf(const char *format, ...)
 		{
 			tab->format++;
 			if (parse(tab) < 0)
+			{
+				free(tab);
 				return (-1);
+			}
 		}
 		else
 			tab->ret = print_char(tab);
